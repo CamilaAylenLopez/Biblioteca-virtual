728a63b6731b1b8de039b738456a421788330d8f
Camila Aylen Lopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2026-03-23 00:56:39 +0100

    Primer commit

---
+++ Backend/config.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+export const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'biblioteca_db',
+};
